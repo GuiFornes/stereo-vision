2873cadc1edfe9fac12e4a02dba4b6331b1933c3
GuiFornes <fornesguillaume@gmail.com>
2022-09-08 15:22:16 -0400

    examples

--- src/5_disp_video.py
+++ src/5_disp_video.py
@@ -1,7 +1,9 @@
 import utils_arducam as arducam
 import time
 import cv2
+import numpy as np
 import utils_stereovision as stereovision
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -11,8 +13,8 @@ if __name__ == "__main__":
         exit()
 
     # Initialisation of the display windows
-    cv2.namedWindow("Stereo")
-    cv2.moveWindow("Stereo", 100, 600)
+    cv2.namedWindow("left rectified")
+    cv2.moveWindow("left rectified", 100, 600)
     cv2.namedWindow("Disparity Map")
     cv2.moveWindow("Disparity Map", 50, 20)
 
@@ -20,6 +22,9 @@ if __name__ == "__main__":
     time0 = time.time()
     count = 0
     matchers = stereovision.Matchers(verbose=False)
+
+    with np.load('calib_data/calibration.npz') as data:
+        leftROI = data['leftROI']
 
     # Capturing loop
     while arducam.running:
@@ -35,14 +40,20 @@ if __name__ == "__main__":
 
         # Compute disparity map
         rectifiedL, rectifiedR = stereovision.rectifyStereoImage(image)
-        cv2.imshow("left rectified", rectifiedL)
-        cv2.imshow("right rectified", rectifiedR)
 
         disparity_map = matchers.computeFilteredDisparityMap(rectifiedL, rectifiedR)
+        # only visual factors operation
+        disparity_map += 24
+        disparity_map *= 2
+        leftROI = (leftROI[0] * 1, leftROI[1] * 1, 960 - (960 - leftROI[2]) * 1, 720 - (720 - leftROI[3]) * 1)
+        disparity_map = disparity_map[leftROI[1]:leftROI[1] + leftROI[3], leftROI[0]:leftROI[0] + leftROI[2]]
+        rectifiedL = rectifiedL[leftROI[1]:leftROI[1] + leftROI[3], leftROI[0]:leftROI[0] + leftROI[2]]
 
         # Show image
-        cv2.imshow("Stereo", image)
-        cv2.imshow("Disparity Map", disparity_map)
+        # cv2.imshow("Stereo", image)
+        cv2.imshow("Disparity Map", cv2.applyColorMap(disparity_map, cv2.COLORMAP_JET))
+        cv2.imshow("left rectified", rectifiedL)
+        # cv2.imshow("right rectified", rectifiedR)
         cv2.waitKey(1)
 
     # Close the camera and release the resources
